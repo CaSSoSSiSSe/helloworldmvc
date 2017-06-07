39b37178e549fc4b22891a5798b9b79a00d66190
CaSSoSSiSSe <yoyo250998@gmail.com>
2017-06-07 15:35:32 +0200

    Doc

--- helloworldmvc/helloworldmvc.Contract/src/main/java/org/helloworldmvc/Contract/IModel.java
+++ helloworldmvc/helloworldmvc.Contract/src/main/java/org/helloworldmvc/Contract/IModel.java
@@ -1,7 +1,16 @@
 package org.helloworldmvc.Contract;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface IModel.
+ */
 public interface IModel {
 
+	/**
+	 * Gets the hello world.
+	 *
+	 * @return the hello world
+	 */
 	public String getHelloWorld();
 
 }
--- helloworldmvc/helloworldmvc.Contract/src/main/java/org/helloworldmvc/Contract/IView.java
+++ helloworldmvc/helloworldmvc.Contract/src/main/java/org/helloworldmvc/Contract/IView.java
@@ -1,7 +1,16 @@
 package org.helloworldmvc.Contract;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface IView.
+ */
 public interface IView {
 
+	/**
+	 * Display message.
+	 *
+	 * @param message the message
+	 */
 	public void displayMessage(String message);
 
 }
